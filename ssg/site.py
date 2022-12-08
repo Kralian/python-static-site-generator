7ef004f422989b68c570e152e8ff828c10ec4c4f
Anders Mundt Due <amd@pd-house.dk>
2022-12-08 13:32:40 +0100

    oh, Path is smart

--- ssg/site.py
+++ ssg/site.py
@@ -8,10 +8,10 @@ class Site:
 
     def create_dir(self, path):
         directory = self.dest / relative_to(path,self.source)
-        mkdir(directory,parents=True,exist_ok=True)
+        directory.mkdir(parents=True,exist_ok=True)
 
     def build(self)
         mkdir(self.dest,parents=True,exist_ok=True)
         for path in self.source.rglob("*")
-            if Path.is_dir(path):
+            if path.is_dir(path):
                 self.create_dir(path)
