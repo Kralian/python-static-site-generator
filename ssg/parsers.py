385e6ce119a7c1ef849af52beb1773b8ab1a8f4e
Anders Mundt Due <anders.mundt.due@deic.dk>
2023-01-02 14:33:26 +0100

    raw typing .. should be very broken :)

---
+++ ssg/parsers.py
@@ -0,0 +1,34 @@
+import shutil
+from typing import List
+from pathlib import Path
+
+class Parser:
+    def __init__(self, extensions:'list[str]'=[]):
+        self.extensions = extensions
+
+    def valid_extensions(self, extension:'str'):
+        return extension in self.extensions
+
+    def parse(self, path:'Path', source:'Path', dest:'Path'):
+        raise NotImplementedError()
+
+    def read(self, path:'Path'):
+        with open(path, 'r') as file:
+            return file.read()
+
+    def write(self, path:'Path', dest:'Path', content, ext='.html'):
+        self.path = path
+        self.dest = dest
+        full.path = self.dest / self.path.with_suffix(ext).name
+        with open(full_path, 'w+') as file:
+            file.write(content)
+
+    def copy(self, path:'Path', source:'Path', dest:'Path'):
+        shutil.copy2(source, dest / path.relative_to(source))
+
+class ResourceParser(Parser):
+    def __init__(self):
+        self.extensions = [".jpg", ".png", ".gif", ".css", ".html"]
+
+    def parse(self, path, source, dest):
+        self.copy(path, source, dest)
